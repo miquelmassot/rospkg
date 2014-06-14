3e91452be9ae96d43fc6952b95bbf1d92f2548df
Miquel Massot <miquel.massot@gmail.com>
2014-06-15 00:11:39 +0100

    Added elementaryOS to os_detect.py

--- src/rospkg/os_detect.py
+++ src/rospkg/os_detect.py
@@ -510,6 +510,7 @@ OS_OSX='osx'
 OS_QNX='qnx'
 OS_RHEL='rhel'
 OS_UBUNTU='ubuntu'
+OS_ELEMENTARY='elementary'
 OS_WINDOWS='windows'
 
 OsDetect.register_default(OS_ARCH, Arch())
@@ -524,6 +525,7 @@ OsDetect.register_default(OS_OSX, OSX())
 OsDetect.register_default(OS_QNX, QNX())
 OsDetect.register_default(OS_RHEL, Rhel())
 OsDetect.register_default(OS_UBUNTU, LsbDetect("Ubuntu"))
+OsDetect.register_default(OS_UBUNTU, LsbDetect('"elementary OS"'))
 OsDetect.register_default(OS_WINDOWS, Windows())    
     
 
